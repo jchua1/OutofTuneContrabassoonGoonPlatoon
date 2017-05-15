37e64211ac0b8b8d2bd5f464febd431ab190cd26
Lawrence Liu <lawrence255@yahoo.com>
2017-05-15 14:05:33 -0400

    basic file structure and flask app

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
-import flask
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/auth")
+def auth():
+    return render_template("bleh.html")
+
+@app.route("/form")#gonna change
+def form():
+    return render_template("stuff.html")
+
+
+if (__name__ == '__main__'):
+    app.debug = True
+
+app.run()
