b8eefbb88cde0640f6fc219761e647612a1eb7d4
Lawrence Liu <lawrence255@yahoo.com>
2017-05-16 13:41:30 -0400

    fixed file names

--- app.py
+++ app.py
@@ -4,18 +4,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("login.html")
 
-@app.route("/auth")
+'''@app.route("/auth")
 def auth():
     return render_template("bleh.html")
-
+'''
 @app.route("/form")#gonna change
 def form():
-    return render_template("stuff.html")
+    return render_template("form.html")
 
 
 if (__name__ == '__main__'):
-    app.debug = True
-
-app.run()
+    #app.debug = True
+    app.run()
