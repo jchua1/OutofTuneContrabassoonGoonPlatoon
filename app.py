b6b33cac2420468d5cae4cb78e23c8077f588bdd
Lawrence Liu <lawrence255@yahoo.com>
2017-05-16 13:59:54 -0400

    basic flask login/auth development

--- app.py
+++ app.py
@@ -1,20 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template #session stuff
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods =['GET', 'POST'])
 def index():
-    return render_template("login.html")
+    return render_template("login.html", msg = "Please login to your account.")
 
-'''@app.route("/auth")
+@app.route("/auth")
 def auth():
-    return render_template("bleh.html")
-'''
-@app.route("/form")#gonna change
+    username = request.form["user"]
+    password = request.form["pw"]
+    if user == "" or password == "":
+        return render_template("login.html", msg="Please enter your username and password.")
+    if ():#username not in database
+        return render_template("login.html", msg = "Username incorrect. Please try again.")
+    if ():#username right, password wrong
+        return render_template("login.html", msg = "Password incorrect. Please try again.")
+    return redirect("/form")
+
+@app.route("/form")
 def form():
     return render_template("form.html")
 
 
-if (__name__ == '__main__'):
-    #app.debug = True
+if __name__ == '__main__':
+    app.debug = True
     app.run()
